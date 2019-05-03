ff1bec515ad6143861068badd2a4266ce11ce6ce
致宇 陳 <pooh810916@gmail.com>
2019-05-03 20:44:22 +0800

    新增遊戲Method

---
+++ Lineage Game/遊戲Method.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            switch (Console.ReadLine().ToString())
+            {
+                case "1":
+                    var x = new Lineage();
+                    x.走路();
+                    break;
+
+                case "2":
+                    var y = new 角色();
+                    y.喝體力藥水();
+                    break;
+
+                case "3":
+                    var z = new Lineage();
+                    z.打怪物();
+                    break;
+            }
+        }
+    }
+}
